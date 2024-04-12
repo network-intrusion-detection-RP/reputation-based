7c0f04035655bb346e6696a4d07bec5d292b5055
nilay nath sharan <nilaynathsharan16@gmail.com>
2024-04-12 06:23:24 +0530

    feat: error handeling in ip tracker

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-
-# IMPORT MODULE
-
+from ReputationManager import ReputationManager
 
 if __name__ == '__main__':
-    pass
+    custom_point_rules = {
+        "country": {"United States": 10, "UK": 20, "India": 100},
+        "region": {"NY": 5, "CA": 8},
+        "city": {"New York City": 5, "Los Angeles": 8},
+        "isp": {"ISP1": 10, "ISP2": 15},
+        "org": {"Organization1": 10, "Organization2": 15}
+    }
+
+    # Initialize ReputationManager with custom point rules
+    reputation_manager = ReputationManager(point_rules=custom_point_rules)
+
+    # Example IP addresses to calculate reputation for
+    ip_addresses = ["136.233.9.98", "8.8.8.8"]
+
+    for ip_address in ip_addresses:
+        # Get reputation score for the IP address
+        reputation_score = reputation_manager.get_reputation(ip_address)
+        print(f"Reputation score for IP address {ip_address}: {reputation_score}")
